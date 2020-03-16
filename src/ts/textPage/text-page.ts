2e3a20b4fc54e7d02634abb5bd8c15b2eac50fe7
danil.khandys <danil.khandys@xpage.ru>
2020-03-16 13:07:07 +0500

    исправил удаление таблиц

--- src/ts/textPage/text-page.ts
+++ src/ts/textPage/text-page.ts
@@ -6,57 +6,57 @@ interface Shadows {
 }
 
 domReady(() => {
-	const tableWrap: HTMLElement = document.createElement("div"),
-		tableWrapTrack: HTMLElement = document.createElement("div"),
-		shadows: Shadows = {
-			right: document.createElement("div"),
-			left: document.createElement("div"),
-		};
+	const tables = App.elementsGetter(".standart-page table:not([class])");
 
-	shadows.right.classList.add("table-wrap__shadow")
-	shadows.right.classList.add("table-wrap__shadow--right")	
-
-	shadows.left.classList.add("table-wrap__shadow")
-	shadows.left.classList.add("table-wrap__shadow--left")
-
-	tableWrap.classList.add("table-wrap");
-	tableWrapTrack.classList.add("table-wrap__track");
-
-	App.wrap(".standart-page table:not([class])", tableWrapTrack)
-	App.wrap(".table-wrap__track", tableWrap)
-
-	App.each(".table-wrap", (el: HTMLElement) => {
-		el.insertBefore(shadows.left, el.querySelector("*:first-child"))
-		el.insertBefore(shadows.right, null)
-	})
-
-	App.each(".table-wrap__track", function(track: HTMLElement){
-		if (track.scrollWidth > track.clientWidth){
-			let wrap = track.closest(".table-wrap");
-
-			let shadows: Shadows = {
-				right: wrap.querySelector(".table-wrap__shadow--right")
+	for (let table of tables){
+		const tableWrap: HTMLElement = document.createElement("div"),
+			tableWrapTrack: HTMLElement = document.createElement("div"),
+			shadows: Shadows = {
+				right: document.createElement("div"),
+				left: document.createElement("div"),
 			};
 
-			setShadowOpacity(shadows.right, track.scrollWidth - track.clientWidth)
-		}
+		shadows.right.classList.add("table-wrap__shadow")
+		shadows.right.classList.add("table-wrap__shadow--right")	
 
-		new EventListener(track).add("scroll", function(el: HTMLElement){
-			const wrap = el.closest(".table-wrap");
+		shadows.left.classList.add("table-wrap__shadow")
+		shadows.left.classList.add("table-wrap__shadow--left")
 
-			let shadows: Shadows = {
-				left: wrap.querySelector(".table-wrap__shadow--left"),
-				right: wrap.querySelector(".table-wrap__shadow--right")
-			};
+		tableWrap.classList.add("table-wrap");
+		tableWrapTrack.classList.add("table-wrap__track");
 
-			setShadowOpacity(shadows.right, el.scrollWidth - el.clientWidth - el.scrollLeft)
-			setShadowOpacity(shadows.left, el.scrollLeft)
-		})
-	})
+		App.wrap(table, tableWrapTrack);
+		App.wrap(tableWrapTrack, tableWrap);
 
+		tableWrap.insertBefore(shadows.left, tableWrap.querySelector("*:first-child"));
+		tableWrap.insertBefore(shadows.right, null);
+
+		;(function(track: HTMLElement){
+			if (track.scrollWidth > track.clientWidth){
+				let wrap = track.closest(".table-wrap");
+
+				let shadows: Shadows = {
+					right: wrap.querySelector(".table-wrap__shadow--right")
+				};
+
+				setShadowOpacity(shadows.right, track.scrollWidth - track.clientWidth)
+			}
+
+			new EventListener(track).add("scroll", function(el: HTMLElement){
+				const wrap = el.closest(".table-wrap");
+
+				let shadows: Shadows = {
+					left: wrap.querySelector(".table-wrap__shadow--left"),
+					right: wrap.querySelector(".table-wrap__shadow--right")
+				};
+
+				setShadowOpacity(shadows.right, el.scrollWidth - el.clientWidth - el.scrollLeft)
+				setShadowOpacity(shadows.left, el.scrollLeft)
+			});
+		})(tableWrapTrack);
+	}
 })
 
 const setShadowOpacity = (element:HTMLElement, scrollWidth: number, offsetWidth: number = 80) => {
-	console.log(123)
 	element.style.opacity = (scrollWidth / offsetWidth <= 1 ? scrollWidth / offsetWidth : 1).toString()
 }
